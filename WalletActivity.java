e73058fac454dfb5b347fc9547cb7494a6589a8a
Bilal Yasin <42803927+bilalyasin1616@users.noreply.github.com>
2019-04-20 12:50:04 +0500

    Added Braintree Working Code

---
+++ WalletActivity.java
@@ -0,0 +1,201 @@
+package com.example.memonasultan.fifaapp;
+
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.braintreepayments.api.dropin.DropInActivity;
+import com.braintreepayments.api.dropin.DropInRequest;
+import com.braintreepayments.api.dropin.DropInResult;
+import com.braintreepayments.api.interfaces.HttpResponseCallback;
+import com.braintreepayments.api.internal.HttpClient;
+import com.braintreepayments.api.models.PaymentMethodNonce;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WalletActivity extends AppCompatActivity {
+
+
+    private static final int REQUEST_CODE = 1234;
+    String API_GET_TOKEN="http://braintree.ngrok.io/braintree/main.php";
+    String API_CHECKOUT="http://braintree.ngrok.io/braintree/checkout.php";
+
+    String token,amount;
+    HashMap<String,String> paramsHash;
+    Button btn_pay;
+    EditText edit_amount;
+    LinearLayout group_payment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wallet);
+
+        edit_amount=(EditText)findViewById(R.id.edit_amount);
+        btn_pay=(Button) findViewById(R.id.btn_pay);
+        group_payment=(LinearLayout)findViewById(R.id.payment_group);
+
+        new WalletActivity.getToken().execute();
+
+        btn_pay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                submitPayment();
+            }
+        });
+
+    }
+
+    private void submitPayment(){
+        String payValue=edit_amount.getText().toString();
+        if(!payValue.isEmpty())
+        {
+            DropInRequest dropInRequest=new DropInRequest().clientToken(token);
+            startActivityForResult(dropInRequest.getIntent(this),REQUEST_CODE);
+        }
+        else
+            Toast.makeText(this, "Enter a valid amount for payment", Toast.LENGTH_SHORT).show();
+
+    }
+
+    private void sendPayments(){
+        RequestQueue queue= Volley.newRequestQueue(WalletActivity.this);
+        StringRequest stringRequest=new StringRequest(Request.Method.POST, API_CHECKOUT,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if(response.toString().contains("Successful")){
+                            Toast.makeText(WalletActivity.this, "Payment Success", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(WalletActivity.this, "Payment Failed", Toast.LENGTH_SHORT).show();
+                        }
+                        Log.d("Response",response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Err",error.toString());
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                if(paramsHash==null)
+                    return null;
+                Map<String,String> params=new HashMap<>();
+                for(String key:paramsHash.keySet())
+                {
+                    params.put(key,paramsHash.get(key));
+                }
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> params=new HashMap<>();
+                params.put("Content-type","application/x-www-form-urlencoded");
+                return params;
+            }
+        };
+        RetryPolicy mRetryPolicy=new DefaultRetryPolicy(0,DefaultRetryPolicy.DEFAULT_MAX_RETRIES,DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        stringRequest.setRetryPolicy(mRetryPolicy);
+        queue.add(stringRequest);
+    }
+
+    private class getToken extends AsyncTask {
+        ProgressDialog mDailog;
+
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            HttpClient client=new HttpClient();
+            client.get(API_GET_TOKEN, new HttpResponseCallback() {
+                @Override
+                public void success(final String responseBody) {
+                    mDailog.dismiss();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            group_payment.setVisibility(View.VISIBLE);
+                            token=responseBody;
+                        }
+                    });
+                }
+
+                @Override
+                public void failure(Exception exception) {
+                    mDailog.dismiss();
+                    Log.d("Err",exception.toString());
+                }
+            });
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mDailog=new ProgressDialog(WalletActivity.this,android.R.style.Theme_DeviceDefault_Light_Dialog);
+            mDailog.setCancelable(false);
+            mDailog.setMessage("Loading Wallet, Please Wait");
+            mDailog.show();
+        }
+
+        @Override
+        protected void onPostExecute(Object o){
+            super.onPostExecute(o);
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode== REQUEST_CODE){
+            if(resultCode==RESULT_OK)
+            {
+                DropInResult result=data.getParcelableExtra(DropInResult.EXTRA_DROP_IN_RESULT);
+                PaymentMethodNonce nonce= result.getPaymentMethodNonce();
+                String strNounce=nonce.getNonce();
+                if(!edit_amount.getText().toString().isEmpty())
+                {
+                    amount=edit_amount.getText().toString();
+                    paramsHash=new HashMap<>();
+                    paramsHash.put("amount",amount);
+                    paramsHash.put("nonce",strNounce);
+
+                    sendPayments();
+                }
+                else {
+                    Toast.makeText(this, "Please enter a valid amount", Toast.LENGTH_SHORT).show();
+                }
+            }
+            else if(resultCode==RESULT_CANCELED)
+            {
+                Toast.makeText(this, "User canceled", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                Exception error=(Exception)data.getSerializableExtra(DropInActivity.EXTRA_ERROR);
+                Log.d("Err",error.toString());
+            }
+        }
+    }
+
+}
